42b0278c913394e976bd4997cbdcd76ad8bebceb
PauloHS8 <paulohenrique62010@gmail.com>
2026-02-11 14:34:31 -0300

    fix: otimizacao do caca-palavras

--- src/api/caca-palavra/controllers/caca-palavra.ts
+++ src/api/caca-palavra/controllers/caca-palavra.ts
@@ -5,42 +5,19 @@ export default factories.createCoreController(
   ({ strapi }) => ({
     async create(ctx) {
       const body = ctx.request.body;
-      const palavras = body.data.palavras;
+      const palavras: string[] = body.data.palavras;
       const N = 8;
 
-      const grid = Array.from({ length: N }, () =>
-        Array.from({ length: N }, () =>
-          String.fromCharCode(65 + Math.floor(Math.random() * 26))
-        )
-      );
-
-      palavras.forEach((palavra: string) => {
-        const vertical = Math.random() < 0.5;
-
-        if (vertical) {
-          const row = Math.floor(Math.random() * (N - palavra.length));
-          const col = Math.floor(Math.random() * N);
-
-          for (let i = 0; i < palavra.length; i++) {
-            grid[row + i][col] = palavra[i].toUpperCase();
-          }
-        } else {
-          const row = Math.floor(Math.random() * N);
-          const col = Math.floor(Math.random() * (N - palavra.length));
-
-          for (let i = 0; i < palavra.length; i++) {
-            grid[row][col + i] = palavra[i].toUpperCase();
-          }
+      function palavraInvalida(palavra: string){
+        if (palavra.length > N) {
+          throw new Error("Palavra maior que a grade!");
         }
-      });
+      }
 
-      body.data.grade = {
-        linhas: N,
-        colunas: N,
-        grade: grid
-      };
+      palavras.forEach (palavra => palavraInvalida(palavra));
+
 
       return await super.create(ctx);
-    }
+    },
   })
 );
